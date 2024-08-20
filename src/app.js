61eb4615e0c14d79e37111797c3111db5f7a4e8e
Harshit Srivastava <harshit.srivastava@geminisolutions.com>
2024-08-21 00:22:13 +0530

    feat: added res for undefined routes

--- src/app.js
+++ src/app.js
@@ -2,4 +2,9 @@ const express = require('express');
 const routes = require('./routes/index')
 const app = express()
 app.use(routes)
+app.use((req,res,next)=>{
+    res.status(404).send({
+        data: 'Not Found'
+    })
+})
 module.exports =app
